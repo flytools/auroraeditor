f5f30278c6fafc53206cbe940b7b03a5b1b21587
Rodrigo Sousa <rodrigosousasim@gmail.com>
2023-08-20 20:12:26 -0300

    mapbox test

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect, useLayoutEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  useLayoutEffect(() => {
+    var bl = 'mapbox://styles/mapbox/streets-v12';
+
+    mapboxgl.accessToken = 'pk.eyJ1Ijoicm9kcmlnb3NpbSIsImEiOiJjbGF2ZngwNzgwNWNhM29qdW9idjl4cWdhIn0.0w3YOTHP8hqYC15F6_D80A';
+    Map = new mapboxgl.Map({
+      container: 'map',
+      style: bl,
+      center: [0, 0],
+      zoom: 3,
+    });
+
+    
+
+  }, []);
+
+
+
+  
 
   return (
     <>
-      <header id="page-header" class="flex flex-none items-center h-16 bg-neutral-900 shadow-sm fixed top-0 right-0 left-0 z-30">
-        <div class="flex justify-between container xl:max-w-6xl mx-auto px-4 lg:px-8">
-          <div class="flex items-center">
-            <a href="" class="group inline-flex items-center space-x-2 font-light text-2xl tracking-wide text-white hover:text-blue-500">
-              <span class="hidden sm:inline">AuroraEditor 1.1</span>
+      <header id="page-header" className="flex flex-none items-center h-16 bg-neutral-900 shadow-sm fixed top-0 right-0 left-0 z-30">
+        <div className="flex justify-between container xl:max-w-6xl mx-auto px-4 lg:px-8">
+          <div className="flex items-center">
+            <a href="" className="group inline-flex items-center space-x-2 font-light text-2xl tracking-wide text-white hover:text-blue-500">
+              <span className="hidden sm:inline">AuroraEditor 1.1</span>
             </a>
           </div>
 
-          <div class="flex items-center space-x-2">
-            <nav class="hidden lg:flex lg:items-center lg:space-x-2 text-sm">
-              <a href="javascript:void(0)" class="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800">
+          <div className="flex items-center space-x-2">
+            <nav className="hidden lg:flex lg:items-center lg:space-x-2 text-sm">
+              <a href="" className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                   <path d="M21 6.375c0 2.692-4.03 4.875-9 4.875S3 9.067 3 6.375 7.03 1.5 12 1.5s9 2.183 9 4.875z" />
                   <path d="M12 12.75c2.685 0 5.19-.586 7.078-1.609a8.283 8.283 0 001.897-1.384c.016.121.025.244.025.368C21 12.817 16.97 15 12 15s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.285 8.285 0 001.897 1.384C6.809 12.164 9.315 12.75 12 12.75z" />
@@ -28,21 +51,21 @@ function App() {
                 <span>Clear</span>
               </a>
 
-              <a href="javascript:void(0)" class="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800">
+              <a href="" className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                   <path d="M9.97.97a.75.75 0 011.06 0l3 3a.75.75 0 01-1.06 1.06l-1.72-1.72v3.44h-1.5V3.31L8.03 5.03a.75.75 0 01-1.06-1.06l3-3zM9.75 6.75v6a.75.75 0 001.5 0v-6h3a3 3 0 013 3v7.5a3 3 0 01-3 3h-7.5a3 3 0 01-3-3v-7.5a3 3 0 013-3h3z" />
                   <path d="M7.151 21.75a2.999 2.999 0 002.599 1.5h7.5a3 3 0 003-3v-7.5c0-1.11-.603-2.08-1.5-2.599v7.099a4.5 4.5 0 01-4.5 4.5H7.151z" />
                 </svg>
                 <span>Import</span>
               </a>
-              <a href="javascript:void(0)" class="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800">
+              <a href="" className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                   <path fillRule="evenodd" d="M9.75 6.75h-3a3 3 0 00-3 3v7.5a3 3 0 003 3h7.5a3 3 0 003-3v-7.5a3 3 0 00-3-3h-3V1.5a.75.75 0 00-1.5 0v5.25zm0 0h1.5v5.69l1.72-1.72a.75.75 0 111.06 1.06l-3 3a.75.75 0 01-1.06 0l-3-3a.75.75 0 111.06-1.06l1.72 1.72V6.75z" clipRule="evenodd" />
                   <path d="M7.151 21.75a2.999 2.999 0 002.599 1.5h7.5a3 3 0 003-3v-7.5c0-1.11-.603-2.08-1.5-2.599v7.099a4.5 4.5 0 01-4.5 4.5H7.151z" />
                 </svg>
                 <span>Export</span>
               </a>
-              <a href="javascript:void(0)" class="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800 bg-neutral-800">
+              <a href="" className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800 bg-neutral-800">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                   <path d="M5.625 3.75a2.625 2.625 0 100 5.25h12.75a2.625 2.625 0 000-5.25H5.625zM3.75 11.25a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5H3.75zM3 15.75a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zM3.75 18.75a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5H3.75z" />
                 </svg>
@@ -51,14 +74,17 @@ function App() {
             </nav>
 
 
-            <div class="lg:hidden">
-              <button type="button" class="text-sm font-medium inline-flex justify-center items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800 focus:outline-none focus:ring" x-on:click="mobileNavOpen = !mobileNavOpen">
-              <svg class="hi-solid hi-menu inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>  
+            <div className="lg:hidden">
+              <button type="button" className="text-sm font-medium inline-flex justify-center items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800 focus:outline-none focus:ring" x-on:click="mobileNavOpen = !mobileNavOpen">
+              <svg className="hi-solid hi-menu inline-block w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>  
               </button>
             </div>
           </div>
         </div>
       </header>
+
+      <div id="map" className="flex flex-none items-center h-full bg-neutral-900 shadow-sm fixed top-0 right-0 left-0 z-50 mt-16">
+      </div>
 
       <h1 className="text-3xl font-bold underline bg-neutral-700">
         AuroraEditor 1
