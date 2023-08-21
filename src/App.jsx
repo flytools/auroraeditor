5e0c36b19ec901acc839e8bb8b26e07c8b305f8c
Rodrigo Sousa <rodrigosousasim@gmail.com>
2023-08-20 23:47:57 -0300

    import modal implements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect, useLayoutEffect } from 'react'
+import { useState, useEffect, useLayoutEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import LayersModal from './components/LayersModal'
+import ImportModal from './components/ImportModal'
 import './App.css'
 import axios from 'axios'
 //https://github.com/naturalatlas/geomagnetism
@@ -16,8 +17,6 @@ var MensureStartCoordinate = null;
 function App() {
 
   BaseLayer = localStorage.getItem("baselayer");
-
-
   useLayoutEffect(() => {
     var bl = 'mapbox://styles/mapbox/streets-v12';
     if (BaseLayer == 'sat') bl = 'mapbox://styles/mapbox/satellite-v9';
@@ -411,6 +410,12 @@ function App() {
   };
 
 
+  const importRef = useRef();
+
+  const handleOpenCloseImport = () => {
+    importRef.current.OpenCloseImport()
+  }
+
 
   
 
@@ -436,7 +441,7 @@ function App() {
                 <span>Clear</span>
               </a>
 
-              <a href="" className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800">
+              <a onClick={handleOpenCloseImport} className="font-medium flex items-center space-x-2 px-3 py-2 rounded text-neutral-400 hover:text-neutral-300 hover:bg-neutral-800">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                   <path d="M9.97.97a.75.75 0 011.06 0l3 3a.75.75 0 01-1.06 1.06l-1.72-1.72v3.44h-1.5V3.31L8.03 5.03a.75.75 0 01-1.06-1.06l3-3zM9.75 6.75v6a.75.75 0 001.5 0v-6h3a3 3 0 013 3v7.5a3 3 0 01-3 3h-7.5a3 3 0 01-3-3v-7.5a3 3 0 013-3h3z" />
                   <path d="M7.151 21.75a2.999 2.999 0 002.599 1.5h7.5a3 3 0 003-3v-7.5c0-1.11-.603-2.08-1.5-2.599v7.099a4.5 4.5 0 01-4.5 4.5H7.151z" />
@@ -477,6 +482,16 @@ function App() {
         SetMensure={SetMensure}
         href="#">
       </LayersModal>
+
+      <ImportModal
+        innerRef={importRef}
+        className="flex flex-none items-center h-full bg-neutral-900 shadow-sm fixed top-0 right-0 left-0 mt-16 z-50"
+        Map={Map}
+        href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </ImportModal>
 
       <h1 className="">
         AuroraEditor 1
